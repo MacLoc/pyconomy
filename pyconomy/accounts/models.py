509c160cf3ac9d9bde8c47a97af7f7174fe92c19
Nicolas Agustin Torres <nicolastrres@gmail.com>
2015-11-28 18:19:19 -0300

    Creating accounts models

--- pyconomy/accounts/models.py
+++ pyconomy/accounts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Account(models.Model):
+    name = models.CharField(max_length=30)
+    type = models.CharField(max_length=30, blank=True)
+
