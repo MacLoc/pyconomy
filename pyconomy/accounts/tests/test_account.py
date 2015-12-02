5353fbe7ce1a745662f0d108fa448e5001d29f9a
Nicolas Agustin Torres <nicolastrres@gmail.com>
2015-12-02 16:44:32 -0300

    Adding get accounts view and tests

--- pyconomy/accounts/tests/test_account.py
+++ pyconomy/accounts/tests/test_account.py
@@ -7,9 +7,7 @@ from rest_framework import status
 from pyconomy.accounts.models import Account
 
 
-
 class AccountTest(APITestCase):
-
     def test_create_account(self):
         account_data = {'name': 'HSBC personal account',
                         'type': 'saving account'}
@@ -17,3 +15,14 @@ class AccountTest(APITestCase):
 
         self.assertEqual(status.HTTP_201_CREATED, response.status_code)
         self.assertEqual(1, Account.objects.count())
+
+    def test_get_account(self):
+        Account.objects.create(name='HSBC personal account',
+                               type='Saving account')
+
+        response = self.client.get('/v1/accounts/')
+
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(1, len(response.data))
+        self.assertEqual('HSBC personal account', response.data[0]['name'])
+        self.assertEqual('Saving account', response.data[0]['type'])
--- pyconomy/accounts/views.py
+++ pyconomy/accounts/views.py
@@ -1,6 +1,8 @@
 from rest_framework.generics import ListCreateAPIView
+from pyconomy.accounts.models import Account
 from pyconomy.accounts.serializers import AccountSerializer
 
 
 class AccountListView(ListCreateAPIView):
     serializer_class = AccountSerializer
+    queryset = Account.objects.all()
