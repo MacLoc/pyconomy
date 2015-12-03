e9b0cdb432ef820bd6d2f647905bab5e073069b7
Nicolas Agustin Torres <nicolastrres@gmail.com>
2015-12-02 23:04:33 -0300

    Adding retrieve income view

--- pyconomy/incomes/tests/test_income.py
+++ pyconomy/incomes/tests/test_income.py
@@ -1,6 +1,7 @@
 import django
 
 from pyconomy.accounts.models import Account
+from pyconomy.incomes.models import Income
 
 django.setup()
 from rest_framework import status
@@ -27,3 +28,26 @@ class IncomeTest(APITestCase):
         self.assertEqual(income_data['category'], response.data['category'])
         self.assertEqual(income_data['account'], response.data['account'])
         self.assertTrue(response.data['is_received'])
+
+    def test_get_income(self):
+        account = Account.objects.create(name='HSBC personal account',
+                                         type='Saving account')
+        income_data = {'description': 'January Salary',
+                       'amount': '1503.90',
+                       'category': 'Salary',
+                       'account': account,
+                       'is_received': True}
+
+        Income.objects.create(**income_data)
+        income_data.update(account=account.id)
+
+        response = self.client.get('/v1/incomes/')
+
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(1, len(response.data))
+
+        # updating the dicts to be able to compare them
+        income_data.update(account=account.id)
+        del response.data[0]['id']
+
+        self.assertDictEqual(income_data, response.data[0])
--- pyconomy/incomes/views.py
+++ pyconomy/incomes/views.py
@@ -1,7 +1,9 @@
 from rest_framework.generics import ListCreateAPIView
+from pyconomy.incomes.models import Income
 
 from pyconomy.incomes.serializers import IncomeSerializer
 
 
 class IncomeListView(ListCreateAPIView):
     serializer_class = IncomeSerializer
+    queryset = Income.objects.all()
